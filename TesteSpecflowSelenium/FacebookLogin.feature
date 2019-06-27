f18c8a7a6d71a0138ceeae7c69e74f04bc202866
CARLOS <luizcssoares@gmail.com>
2019-06-27 12:03:13 -0300

    commit inicial

---
+++ TesteSpecflowSelenium/FacebookLogin.feature
@@ -0,0 +1,7 @@
+﻿#language: pt
+Funcionalidade: Testar Login no Facebook 
+ 
+Cenario: Testar Login com credenciais validas
+Dado Abra Navegador e Start Aplicacao
+Quando Eu entro com usuario e password validas
+Entao Usuario deve fazer Login com sucesso
---
+++ TesteSpecflowSelenium/TestarLoginNoFacebookSteps.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TesteSpecflowSelenium
+{
+    [Binding]
+    public class TestarLoginNoFacebookSteps
+    {
+
+        IWebDriver browser;
+        private string url = "https://www.facebook.com/";
+
+        [BeforeScenario]
+        public void Init() {
+            this.browser = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void Close() {
+            this.browser.Close();
+            this.browser.Dispose();
+        }
+
+        [Given(@"Abra Navegador e Start Aplicacao")]
+        public void DadoAbraNavegadorEStartAplicacao()
+        {
+            this.browser.Navigate().GoToUrl(url);            
+        }
+        
+        [When(@"Eu entro com usuario e password validas")]
+        public void QuandoEuEntroComUsuarioEPasswordValidas()
+        {
+            this.browser.FindElement(By.Id("email")).SendKeys("luizcssoares@gmail.com");
+            this.browser.FindElement(By.Id("pass")).SendKeys("lucas@2001");
+        }
+        
+        [Then(@"Usuario deve fazer Login com sucesso")]
+        public void EntaoUsuarioDeveFazerLoginComSucesso()
+        {
+            var btnLogin = this.browser.FindElement(By.Id("loginbutton"));
+            btnLogin.Click();
+        }
+    }
+}
